d4b8a85ad9e6197c36c6586365c297176d7bd4ca
HoffsMH <matthecker@pm.me>
2019-09-07 00:49:36 -0400

    sort em right

--- config.h
+++ config.h
@@ -33,6 +33,9 @@ static const Rule rules[] = {
   /* class      instance    title       tags mask     isfloating   monitor */
   { "Gimp",     NULL,       NULL,       0,            1,           -1 },
   { "firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
+  { "Slack",  NULL,       NULL,         1 << 3,       0,           -1 },
+  { "discord",  NULL,       NULL,       1 << 3,       0,           -1 },
+  { "chrome",  NULL,       NULL,        1 << 1,       0,           -1 },
   { "Peek",     NULL,       NULL,       0,            1,           -1 },
 };
 
